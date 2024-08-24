dakurei-gems/discordrb

Replace all RestClient by Faraday
  + As this commit stands, breaking change absolute

lib/discordrb/api.rb
@@ -73,18 +73,18 @@ module Discordrb::API
     mutex.unlock
   end
 
-  # Performs a RestClient request.
+  # Performs a Faraday request.
   # @param type [Symbol] The type of HTTP request to use.
   # @param attributes [Array] The attributes for the request.
   def raw_request(type, attributes)
-    RestClient.send(type, *attributes)
-  rescue RestClient::Forbidden => e
-    # HACK: for #request, dynamically inject restclient's response into NoPermission - this allows us to rate limit
+    Faraday.send(type, *attributes)
+  rescue Faraday::ForbiddenError => e
+    # HACK: for #request, dynamically inject faraday's response into NoPermission - this allows us to rate limit
     noprm = Discordrb::Errors::NoPermission.new
     noprm.define_singleton_method(:_rc_response) { e.response }
     raise noprm, "The bot doesn't have the required permission to do this!"
-  rescue RestClient::BadGateway
-    Discordrb::LOGGER.warn("Got a 502 while sending a request! Not a big deal, retrying the request")
+  rescue Faraday::ServerError
+    Discordrb::LOGGER.warn("Got a 5xx while sending a request! Not a big deal, retrying the request")
     retry
   end
 
@@ -109,10 +109,10 @@ module Discordrb::API
       response = nil
       begin
         response = raw_request(type, attributes)
-      rescue RestClient::Exception => e
+      rescue Faraday::Error => e
         response = e.response
 
-        if response.body && !e.is_a?(RestClient::TooManyRequests)
+        if response.body && !e.is_a?(Faraday::TooManyRequestsError)
           data = JSON.parse(response.body)
           err_klass = Discordrb::Errors.error_class_for(data["code"] || 0)
           e = err_klass.new(data["message"], data["errors"])
@@ -136,7 +136,7 @@ module Discordrb::API
           Discordrb::LOGGER.ratelimit("Response was nil before trying to preemptively rate limit!")
         end
       end
-    rescue RestClient::TooManyRequests => e
+    rescue Faraday::TooManyRequestsError => e
       # If the 429 is from the global RL, then we have to use the global mutex instead.
       mutex = @global_mutex if e.response.headers[:x_ratelimit_global] == "true"
 

lib/discordrb/api/channel.rb
@@ -93,7 +93,7 @@ module Discordrb::API::Channel
       body,
       **headers
     )
-  rescue RestClient::BadRequest => e
+  rescue Faraday::BadRequestError => e
     parsed = JSON.parse(e.response.body)
     raise Discordrb::Errors::MessageTooLong, "Message over the character limit (#{message.length} > 2000)" if parsed["content"].is_a?(Array) && parsed["content"].first == "Must be 2000 or fewer in length."
 
@@ -362,7 +362,7 @@ module Discordrb::API::Channel
   # @deprecated Discord no longer supports bots in group DMs, this endpoint was repurposed and no longer works as implemented here.
   # https://discord.com/developers/docs/resources/channel#group-dm-add-recipient
   def create_group(token, pm_channel_id, user_id)
-    Discordrb::API.request(
+    response = Discordrb::API.request(
       :channels_cid_recipients_uid,
       nil,
       :put,
@@ -371,11 +371,10 @@ module Discordrb::API::Channel
       Authorization: token,
       content_type: :json
     )
-  rescue RestClient::InternalServerError
+    raise("Attempted to create a group channel with the PM channel recipient!") if response.status == 204
+  rescue Faraday::ServerError
     raise "Attempted to add self as a new group channel recipient!"
-  rescue RestClient::NoContent
-    raise "Attempted to create a group channel with the PM channel recipient!"
-  rescue RestClient::Forbidden
+  rescue Faraday::ForbiddenError
     raise "Attempted to add a user to group channel without permission!"
   end
 

dakurei-gems/rubycord

lib/rubycord/api.rb
@@ -73,18 +73,18 @@ module Rubycord::API
     mutex.unlock
   end
 
-  # Performs a RestClient request.
+  # Performs a Faraday request.
   # @param type [Symbol] The type of HTTP request to use.
   # @param attributes [Array] The attributes for the request.
   def raw_request(type, attributes)
-    RestClient.send(type, *attributes)
-  rescue RestClient::Forbidden => e
-    # HACK: for #request, dynamically inject restclient's response into NoPermission - this allows us to rate limit
+    Faraday.send(type, *attributes)
+  rescue Faraday::ForbiddenError => e
+    # HACK: for #request, dynamically inject faraday's response into NoPermission - this allows us to rate limit
     noprm = Rubycord::Errors::NoPermission.new
     noprm.define_singleton_method(:_rc_response) { e.response }
     raise noprm, "The bot doesn't have the required permission to do this!"
-  rescue RestClient::BadGateway
-    Rubycord::LOGGER.warn("Got a 502 while sending a request! Not a big deal, retrying the request")
+  rescue Faraday::ServerError
+    Rubycord::LOGGER.warn("Got a 5xx while sending a request! Not a big deal, retrying the request")
     retry
   end
 
@@ -109,10 +109,10 @@ module Rubycord::API
       response = nil
       begin
         response = raw_request(type, attributes)
-      rescue RestClient::Exception => e
+      rescue Faraday::Error => e
         response = e.response
 
-        if response.body && !e.is_a?(RestClient::TooManyRequests)
+        if response.body && !e.is_a?(Faraday::TooManyRequestsError)
           data = JSON.parse(response.body)
           err_klass = Rubycord::Errors.error_class_for(data["code"] || 0)
           e = err_klass.new(data["message"], data["errors"])
@@ -136,7 +136,7 @@ module Rubycord::API
           Rubycord::LOGGER.ratelimit("Response was nil before trying to preemptively rate limit!")
         end
       end
-    rescue RestClient::TooManyRequests => e
+    rescue Faraday::TooManyRequestsError => e
       # If the 429 is from the global RL, then we have to use the global mutex instead.
       mutex = @global_mutex if e.response.headers[:x_ratelimit_global] == "true"
 

lib/rubycord/api/channel.rb
@@ -93,7 +93,7 @@ module Rubycord::API::Channel
       body,
       **headers
     )
-  rescue RestClient::BadRequest => e
+  rescue Faraday::BadRequestError => e
     parsed = JSON.parse(e.response.body)
     raise Rubycord::Errors::MessageTooLong, "Message over the character limit (#{message.length} > 2000)" if parsed["content"].is_a?(Array) && parsed["content"].first == "Must be 2000 or fewer in length."
 
@@ -362,7 +362,7 @@ module Rubycord::API::Channel
   # @deprecated Discord no longer supports bots in group DMs, this endpoint was repurposed and no longer works as implemented here.
   # https://discord.com/developers/docs/resources/channel#group-dm-add-recipient
   def create_group(token, pm_channel_id, user_id)
-    Rubycord::API.request(
+    response = Rubycord::API.request(
       :channels_cid_recipients_uid,
       nil,
       :put,
@@ -371,11 +371,10 @@ module Rubycord::API::Channel
       Authorization: token,
       content_type: :json
     )
-  rescue RestClient::InternalServerError
+    raise("Attempted to create a group channel with the PM channel recipient!") if response.status == 204
+  rescue Faraday::ServerError
     raise "Attempted to add self as a new group channel recipient!"
-  rescue RestClient::NoContent
-    raise "Attempted to create a group channel with the PM channel recipient!"
-  rescue RestClient::Forbidden
+  rescue Faraday::ForbiddenError
     raise "Attempted to add a user to group channel without permission!"
   end
 