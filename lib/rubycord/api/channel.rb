dakurei-gems/discordrb

Cleanup - Remove unnecessary hard coded query strings in GET requests

lib/discordrb/api/channel.rb
@@ -49,13 +49,12 @@ module Discordrb::API::Channel
   # Get a list of messages from a channel's history
   # https://discord.com/developers/docs/resources/channel#get-channel-messages
   def messages(token, channel_id, amount, before = nil, after = nil, around = nil)
-    query_string = URI.encode_www_form({limit: amount, before: before, after: after, around: around}.compact)
     Discordrb::API.request(
       :channels_cid_messages,
       channel_id,
       :get,
-      "#{Discordrb::API.api_base}/channels/#{channel_id}/messages?#{query_string}",
-      nil,
+      "#{Discordrb::API.api_base}/channels/#{channel_id}/messages",
+      {limit: amount, before: before, after: after, around: around}.compact,
       authorization: token
     )
   end
@@ -207,13 +206,12 @@ module Discordrb::API::Channel
   # https://discord.com/developers/docs/resources/channel#get-reactions
   def get_reactions(token, channel_id, message_id, emoji, before_id, after_id, limit = 100)
     emoji = URI.encode_www_form_component(emoji) unless emoji.ascii_only?
-    query_string = URI.encode_www_form({limit: limit || 100, before: before_id, after: after_id}.compact)
     Discordrb::API.request(
       :channels_cid_messages_mid_reactions_emoji,
       channel_id,
       :get,
-      "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/#{emoji}?#{query_string}",
-      nil,
+      "#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/#{emoji}",
+      {limit: limit || 100, before: before_id, after: after_id}.compact,
       authorization: token
     )
   end
@@ -548,14 +546,12 @@ module Discordrb::API::Channel
   # Get the members of a thread.
   # https://discord.com/developers/docs/resources/channel#list-thread-members
   def list_thread_members(token, channel_id, before, limit)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Discordrb::API.request(
       :channels_cid_thread_members,
       channel_id,
       :get,
-      "#{Discordrb::API.api_base}/channels/#{channel_id}/thread-members?#{query}",
-      nil,
+      "#{Discordrb::API.api_base}/channels/#{channel_id}/thread-members",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end
@@ -576,14 +572,12 @@ module Discordrb::API::Channel
   # List public archived threads.
   # https://discord.com/developers/docs/resources/channel#list-public-archived-threads
   def list_public_archived_threads(token, channel_id, before = nil, limit = nil)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Discordrb::API.request(
       :channels_cid_threads_archived_public,
       channel_id,
       :get,
-      "#{Discordrb::API.api_base}/channels/#{channel_id}/threads/archived/public?#{query}",
-      nil,
+      "#{Discordrb::API.api_base}/channels/#{channel_id}/threads/archived/public",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end
@@ -591,14 +585,12 @@ module Discordrb::API::Channel
   # List private archived threads.
   # https://discord.com/developers/docs/resources/channel#list-private-archived-threads
   def list_private_archived_threads(token, channel_id, before = nil, limit = nil)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Discordrb::API.request(
       :channels_cid_threads_archived_private,
       channel_id,
       :get,
-      "#{Discordrb::API.api_base}/channels/#{channel_id}/threads/archived/private?#{query}",
-      nil,
+      "#{Discordrb::API.api_base}/channels/#{channel_id}/threads/archived/private",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end
@@ -606,14 +598,12 @@ module Discordrb::API::Channel
   # List joined private archived threads.
   # https://discord.com/developers/docs/resources/channel#list-joined-private-archived-threads
   def list_joined_private_archived_threads(token, channel_id, before = nil, limit = nil)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Discordrb::API.request(
       :channels_cid_users_me_threads_archived_private,
       channel_id,
       :get,
-      "#{Discordrb::API.api_base}/channels/#{channel_id}/users/@me/threads/archived/private?#{query}",
-      nil,
+      "#{Discordrb::API.api_base}/channels/#{channel_id}/users/@me/threads/archived/private",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end

lib/discordrb/api/invite.rb
@@ -9,8 +9,8 @@ module Discordrb::API::Invite
       :invite_code,
       nil,
       :get,
-      "#{Discordrb::API.api_base}/invites/#{invite_code}#{counts ? "?with_counts=true" : ""}",
-      nil,
+      "#{Discordrb::API.api_base}/invites/#{invite_code}",
+      {with_counts: counts.is_a?(TrueClass) ? true : nil}.compact,
       authorization: token
     )
   end

lib/discordrb/api/server.rb
@@ -23,8 +23,8 @@ module Discordrb::API::Server
       :guilds_sid,
       server_id,
       :get,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}#{"?with_counts=true" if with_counts}",
-      nil,
+      "#{Discordrb::API.api_base}/guilds/#{server_id}",
+      {with_counts: with_counts.is_a?(TrueClass) ? true : nil}.compact,
       authorization: token
     )
   end
@@ -130,13 +130,12 @@ module Discordrb::API::Server
   # Gets members from the server
   # https://discord.com/developers/docs/resources/guild#list-guild-members
   def resolve_members(token, server_id, limit, after = nil)
-    query_string = URI.encode_www_form({limit: limit, after: after}.compact)
     Discordrb::API.request(
       :guilds_sid_members,
       server_id,
       :get,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}/members?#{query_string}",
-      nil,
+      "#{Discordrb::API.api_base}/guilds/#{server_id}/members",
+      {limit: limit, after: after}.compact,
       authorization: token
     )
   end
@@ -182,13 +181,12 @@ module Discordrb::API::Server
   # Get a server's banned users
   # https://discord.com/developers/docs/resources/guild#get-guild-bans
   def bans(token, server_id, limit = nil, before = nil, after = nil)
-    query_string = URI.encode_www_form({limit: limit, before: before, after: after}.compact)
     Discordrb::API.request(
       :guilds_sid_bans,
       server_id,
       :get,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}/bans?#{query_string}",
-      nil,
+      "#{Discordrb::API.api_base}/guilds/#{server_id}/bans",
+      {limit: limit, before: before, after: after}.compact,
       authorization: token
     )
   end
@@ -348,8 +346,8 @@ module Discordrb::API::Server
       :guilds_sid_prune,
       server_id,
       :get,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}/prune?days=#{days}",
-      nil,
+      "#{Discordrb::API.api_base}/guilds/#{server_id}/prune",
+      {days: days},
       authorization: token
     )
   end
@@ -389,8 +387,8 @@ module Discordrb::API::Server
       :guilds_sid_auditlogs,
       server_id,
       :get,
-      "#{Discordrb::API.api_base}/guilds/#{server_id}/audit-logs?limit=#{limit}#{"&user_id=#{user_id}" if user_id}#{"&action_type=#{action_type}" if action_type}#{"&before=#{before}" if before}",
-      nil,
+      "#{Discordrb::API.api_base}/guilds/#{server_id}/audit-logs",
+      {limit: limit, user_id: user_id, action_type: action_type, before: before}.compact,
       authorization: token
     )
   end

dakurei-gems/rubycord

lib/rubycord/api/channel.rb
@@ -49,13 +49,12 @@ module Rubycord::API::Channel
   # Get a list of messages from a channel's history
   # https://discord.com/developers/docs/resources/channel#get-channel-messages
   def messages(token, channel_id, amount, before = nil, after = nil, around = nil)
-    query_string = URI.encode_www_form({limit: amount, before: before, after: after, around: around}.compact)
     Rubycord::API.request(
       :channels_cid_messages,
       channel_id,
       :get,
-      "#{Rubycord::API.api_base}/channels/#{channel_id}/messages?#{query_string}",
-      nil,
+      "#{Rubycord::API.api_base}/channels/#{channel_id}/messages",
+      {limit: amount, before: before, after: after, around: around}.compact,
       authorization: token
     )
   end
@@ -207,13 +206,12 @@ module Rubycord::API::Channel
   # https://discord.com/developers/docs/resources/channel#get-reactions
   def get_reactions(token, channel_id, message_id, emoji, before_id, after_id, limit = 100)
     emoji = URI.encode_www_form_component(emoji) unless emoji.ascii_only?
-    query_string = URI.encode_www_form({limit: limit || 100, before: before_id, after: after_id}.compact)
     Rubycord::API.request(
       :channels_cid_messages_mid_reactions_emoji,
       channel_id,
       :get,
-      "#{Rubycord::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/#{emoji}?#{query_string}",
-      nil,
+      "#{Rubycord::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/#{emoji}",
+      {limit: limit || 100, before: before_id, after: after_id}.compact,
       authorization: token
     )
   end
@@ -548,14 +546,12 @@ module Rubycord::API::Channel
   # Get the members of a thread.
   # https://discord.com/developers/docs/resources/channel#list-thread-members
   def list_thread_members(token, channel_id, before, limit)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Rubycord::API.request(
       :channels_cid_thread_members,
       channel_id,
       :get,
-      "#{Rubycord::API.api_base}/channels/#{channel_id}/thread-members?#{query}",
-      nil,
+      "#{Rubycord::API.api_base}/channels/#{channel_id}/thread-members",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end
@@ -576,14 +572,12 @@ module Rubycord::API::Channel
   # List public archived threads.
   # https://discord.com/developers/docs/resources/channel#list-public-archived-threads
   def list_public_archived_threads(token, channel_id, before = nil, limit = nil)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Rubycord::API.request(
       :channels_cid_threads_archived_public,
       channel_id,
       :get,
-      "#{Rubycord::API.api_base}/channels/#{channel_id}/threads/archived/public?#{query}",
-      nil,
+      "#{Rubycord::API.api_base}/channels/#{channel_id}/threads/archived/public",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end
@@ -591,14 +585,12 @@ module Rubycord::API::Channel
   # List private archived threads.
   # https://discord.com/developers/docs/resources/channel#list-private-archived-threads
   def list_private_archived_threads(token, channel_id, before = nil, limit = nil)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Rubycord::API.request(
       :channels_cid_threads_archived_private,
       channel_id,
       :get,
-      "#{Rubycord::API.api_base}/channels/#{channel_id}/threads/archived/private?#{query}",
-      nil,
+      "#{Rubycord::API.api_base}/channels/#{channel_id}/threads/archived/private",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end
@@ -606,14 +598,12 @@ module Rubycord::API::Channel
   # List joined private archived threads.
   # https://discord.com/developers/docs/resources/channel#list-joined-private-archived-threads
   def list_joined_private_archived_threads(token, channel_id, before = nil, limit = nil)
-    query = URI.encode_www_form({before: before, limit: limit}.compact)
-
     Rubycord::API.request(
       :channels_cid_users_me_threads_archived_private,
       channel_id,
       :get,
-      "#{Rubycord::API.api_base}/channels/#{channel_id}/users/@me/threads/archived/private?#{query}",
-      nil,
+      "#{Rubycord::API.api_base}/channels/#{channel_id}/users/@me/threads/archived/private",
+      {before: before, limit: limit}.compact,
       authorization: token
     )
   end

lib/rubycord/api/invite.rb
@@ -9,8 +9,8 @@ module Rubycord::API::Invite
       :invite_code,
       nil,
       :get,
-      "#{Rubycord::API.api_base}/invites/#{invite_code}#{counts ? "?with_counts=true" : ""}",
-      nil,
+      "#{Rubycord::API.api_base}/invites/#{invite_code}",
+      {with_counts: counts.is_a?(TrueClass) ? true : nil}.compact,
       authorization: token
     )
   end

lib/rubycord/api/server.rb
@@ -23,8 +23,8 @@ module Rubycord::API::Server
       :guilds_sid,
       server_id,
       :get,
-      "#{Rubycord::API.api_base}/guilds/#{server_id}#{"?with_counts=true" if with_counts}",
-      nil,
+      "#{Rubycord::API.api_base}/guilds/#{server_id}",
+      {with_counts: with_counts.is_a?(TrueClass) ? true : nil}.compact,
       authorization: token
     )
   end
@@ -130,13 +130,12 @@ module Rubycord::API::Server
   # Gets members from the server
   # https://discord.com/developers/docs/resources/guild#list-guild-members
   def resolve_members(token, server_id, limit, after = nil)
-    query_string = URI.encode_www_form({limit: limit, after: after}.compact)
     Rubycord::API.request(
       :guilds_sid_members,
       server_id,
       :get,
-      "#{Rubycord::API.api_base}/guilds/#{server_id}/members?#{query_string}",
-      nil,
+      "#{Rubycord::API.api_base}/guilds/#{server_id}/members",
+      {limit: limit, after: after}.compact,
       authorization: token
     )
   end
@@ -182,13 +181,12 @@ module Rubycord::API::Server
   # Get a server's banned users
   # https://discord.com/developers/docs/resources/guild#get-guild-bans
   def bans(token, server_id, limit = nil, before = nil, after = nil)
-    query_string = URI.encode_www_form({limit: limit, before: before, after: after}.compact)
     Rubycord::API.request(
       :guilds_sid_bans,
       server_id,
       :get,
-      "#{Rubycord::API.api_base}/guilds/#{server_id}/bans?#{query_string}",
-      nil,
+      "#{Rubycord::API.api_base}/guilds/#{server_id}/bans",
+      {limit: limit, before: before, after: after}.compact,
       authorization: token
     )
   end
@@ -348,8 +346,8 @@ module Rubycord::API::Server
       :guilds_sid_prune,
       server_id,
       :get,
-      "#{Rubycord::API.api_base}/guilds/#{server_id}/prune?days=#{days}",
-      nil,
+      "#{Rubycord::API.api_base}/guilds/#{server_id}/prune",
+      {days: days},
       authorization: token
     )
   end
@@ -389,8 +387,8 @@ module Rubycord::API::Server
       :guilds_sid_auditlogs,
       server_id,
       :get,
-      "#{Rubycord::API.api_base}/guilds/#{server_id}/audit-logs?limit=#{limit}#{"&user_id=#{user_id}" if user_id}#{"&action_type=#{action_type}" if action_type}#{"&before=#{before}" if before}",
-      nil,
+      "#{Rubycord::API.api_base}/guilds/#{server_id}/audit-logs",
+      {limit: limit, user_id: user_id, action_type: action_type, before: before}.compact,
       authorization: token
     )
   end