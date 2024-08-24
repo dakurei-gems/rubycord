dakurei-gems/discordrb

Update user_agent to use Faraday

lib/discordrb/api.rb
@@ -53,7 +53,7 @@ module Discordrb::API
     required = "DiscordBot (https://github.com/dakurei-gems/discordrb, v#{Discordrb::VERSION})"
     @bot_name ||= ""
 
-    "#{required} rest-client/#{RestClient::VERSION} #{RUBY_ENGINE}/#{RUBY_VERSION}p#{RUBY_PATCHLEVEL} discordrb/#{Discordrb::VERSION} #{@bot_name}".strip
+    "#{required} faraday/#{Faraday::VERSION} #{RUBY_ENGINE}/#{RUBY_VERSION}p#{RUBY_PATCHLEVEL} discordrb/#{Discordrb::VERSION} #{@bot_name}".strip
   end
 
   # Resets all rate limit mutexes

dakurei-gems/rubycord

lib/rubycord/api.rb
@@ -53,7 +53,7 @@ module Rubycord::API
     required = "DiscordBot (https://github.com/dakurei-gems/rubycord, v#{Rubycord::VERSION})"
     @bot_name ||= ""
 
-    "#{required} rest-client/#{RestClient::VERSION} #{RUBY_ENGINE}/#{RUBY_VERSION}p#{RUBY_PATCHLEVEL} rubycord/#{Rubycord::VERSION} #{@bot_name}".strip
+    "#{required} faraday/#{Faraday::VERSION} #{RUBY_ENGINE}/#{RUBY_VERSION}p#{RUBY_PATCHLEVEL} rubycord/#{Rubycord::VERSION} #{@bot_name}".strip
   end
 
   # Resets all rate limit mutexes